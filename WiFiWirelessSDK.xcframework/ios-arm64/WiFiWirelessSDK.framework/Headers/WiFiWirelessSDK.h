daf315073ff2d6407cb729a8d08a9c36a71ad438
Sarath P <sarath@pardypanda.com>
2024-05-23 19:14:30 +0530

    framework added

---
+++ WiFiWirelessSDK.xcframework/ios-arm64/WiFiWirelessSDK.framework/Headers/WiFiWirelessSDK.h
@@ -0,0 +1,18 @@
+//
+//  WiFiWirelessSDK.h
+//  WiFiWirelessSDK
+//
+//  Created by PPS SARATH M2 on 5/23/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WiFiWirelessSDK.
+FOUNDATION_EXPORT double WiFiWirelessSDKVersionNumber;
+
+//! Project version string for WiFiWirelessSDK.
+FOUNDATION_EXPORT const unsigned char WiFiWirelessSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WiFiWirelessSDK/PublicHeader.h>
+
+
